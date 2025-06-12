06405eef3850428efd3ab5e118506e8ea0f6598e
ahonore42 <a1honore80@gmail.com>
2025-06-12 15:48:36 -0700

    feat: add theme provider to app instantiation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import type { AppType, AppProps } from 'next/app';
 import type { ReactElement, ReactNode } from 'react';
 
+import { ThemeProvider } from '~/lib/contexts/ThemeContext';
 import { DefaultLayout } from '~/components/DefaultLayout';
 import { trpc } from '~/utils/trpc';
 import '~/styles/globals.css';
@@ -21,7 +22,11 @@ const MyApp = (({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout =
     Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
 
-  return getLayout(<Component {...pageProps} />);
+    return (
+      <ThemeProvider defaultTheme="light">
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
+    )
 }) as AppType;
 
 export default trpc.withTRPC(MyApp);
