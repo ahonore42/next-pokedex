e59e65ddda09702588087b98857ab66863f5490c
ahonore42 <a1honore80@gmail.com>
2025-06-12 18:36:26 -0700

    feat: move pokemon utility functions to utils

---
+++ src/utils/pokemon.ts
@@ -0,0 +1,34 @@
+// Utility function to capitalize Pokemon names
+export function capitalizeName(name: string): string {
+  return name
+    .split('-')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+// Utility function to get type color - Complete list
+export function getTypeColor(type: string): string {
+  const colors: Record<string, string> = {
+    normal: '#A8A878',
+    fighting: '#C03028',
+    flying: '#A890F0',
+    poison: '#A040A0',
+    ground: '#E0C068',
+    rock: '#B8A038',
+    bug: '#A8B820',
+    ghost: '#705898',
+    steel: '#B8B8D0',
+    fire: '#F08030',
+    water: '#6890F0',
+    grass: '#78C850',
+    electric: '#F8D030',
+    psychic: '#F85888',
+    ice: '#98D8D8',
+    dragon: '#7038F8',
+    dark: '#705848',
+    fairy: '#EE99AC',
+    stellar: '#40E0D0',
+    unknown: '#68A090',
+  };
+  return colors[type] || '#68A090';
+}
