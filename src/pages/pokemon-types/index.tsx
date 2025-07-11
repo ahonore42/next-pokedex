75bd02207767115a7ecfe33b913f9754c8abc1c6
ahonore42 <a1honore80@gmail.com>
2025-07-11 13:02:57 -0700

    feat: Display TypeEffectivenessChart on types page
    This commit enhances the Pokémon types page by adding the TypeEffectivenessChart component, which provides a comprehensive overview of type matchups. The tRPC hook for fetching types has also been updated to align with the new router structure.

--- src/pages/pokemon-types/index.tsx
+++ src/pages/pokemon-types/index.tsx
@@ -3,20 +3,26 @@ import SectionCard from '~/components/ui/SectionCard';
 import { TypeBadge } from '~/components/ui/TypeBadge';
 import { trpc } from '~/utils/trpc';
 import LoadingPage from '~/components/ui/LoadingPage';
+import TypeEffectivenessChart from '~/components/pokemon/TypeEffectivenessChart';
 
 const TypesPage: NextPage = () => {
-  const { data: types, isLoading } = trpc.pokemon.allTypes.useQuery();
+  const { data: types, isLoading } = trpc.types.allTypes.useQuery();
+
   if (isLoading || !types) {
     return <LoadingPage />;
   }
   return (
-    <SectionCard title="Pokémon Types">
-      <div className="flex flex-wrap gap-4 justify-center">
-        {types.map((type) => (
-          <TypeBadge key={type.id} type={type} />
-        ))}
-      </div>
-    </SectionCard>
+    <>
+      <SectionCard title="Pokémon Types" className="mb-4">
+        <div className="flex flex-wrap gap-4 justify-center">
+          {types.map((type) => (
+            <TypeBadge key={type.id} type={type} />
+          ))}
+        </div>
+      </SectionCard>
+
+      <TypeEffectivenessChart />
+    </>
   );
 };
 
