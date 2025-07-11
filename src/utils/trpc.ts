b28487b52fcdc16b07f1650fae9958bdee5d7555
ahonore42 <a1honore80@gmail.com>
2025-07-11 13:24:24 -0700

    chore: Comment out tRPC loggerLink

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -57,11 +57,11 @@ export const trpc = createTRPCNext<AppRouter, SSRContext>({
        */
       links: [
         // adds pretty logs to your console in development and logs errors in production
-        loggerLink({
-          enabled: (opts) =>
-            process.env.NODE_ENV === 'development' ||
-            (opts.direction === 'down' && opts.result instanceof Error),
-        }),
+        // loggerLink({
+        //   enabled: (opts) =>
+        //     process.env.NODE_ENV === 'development' ||
+        //     (opts.direction === 'down' && opts.result instanceof Error),
+        // }),
         httpBatchStreamLink({
           url: `${getBaseUrl()}/api/trpc`,
           /**
