edea7da25ce412ef339b5e2a3bc4af5bd8208959
ahonore42 <a1honore80@gmail.com>
2025-07-15 22:26:14 -0700

    feat: integrate LoadingProvider into _app.tsx
    Wraps the application with LoadingProvider in _app.tsx to enable global and page-specific loading state management.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next';
 import type { AppType, AppProps } from 'next/app';
-import Head from 'next/head'; // Import Head component
-
+import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
 import { DefaultLayout } from '~/components/layout/DefaultLayout';
+import { LoadingProvider } from '~/lib/contexts/LoadingContext';
 import { trpc } from '~/utils/trpc';
 import '~/styles/globals.css';
 import '@xyflow/react/dist/style.css';
@@ -26,9 +26,11 @@ const MyApp = (({ Component, pageProps }: AppPropsWithLayout) => {
         <link rel="dns-prefetch" href="https://raw.githubusercontent.com" />
       </Head>
       <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-        <DefaultLayout>
-          <Component {...pageProps} />
-        </DefaultLayout>
+        <LoadingProvider>
+          <DefaultLayout>
+            <Component {...pageProps} />
+          </DefaultLayout>
+        </LoadingProvider>
       </ThemeProvider>
     </>
   );
