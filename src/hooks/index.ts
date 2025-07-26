5ee9aa584cf64df44299c657dafc5212552bf675
ahonore42 <a1honore80@gmail.com>
2025-07-25 17:10:53 -0700

    chore(hooks): move hooks to root directory
    Moved all hooks from src/lib/hooks to src/hooks. Added new exports for generation filtering.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,9 @@
 export { useBreakpointWidth } from './useBreakpointWidth';
 export { useReactFlowFitView } from './useReactFlowFitView';
 export { useElementHeight } from './useElementHeight';
+export {
+  useGenerationFilter,
+  pokemonFlavorTextsConfig,
+  pokemonMovesConfig,
+  pokemonEncountersConfig,
+} from './useGenerationFilter';
