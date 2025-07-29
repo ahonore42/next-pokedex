c76a8637ff97de5a178abb259f5f717742ff63d6
ahonore42 <a1honore80@gmail.com>
2025-07-28 23:53:02 -0700

    feat(skeletons): add index file for skeleton components
    Centralizes exports for various skeleton components, including base skeletons, specific UI element skeletons, and preset compositions.

---
+++ src/components/ui/skeletons/index.ts
@@ -0,0 +1,19 @@
+// Base skeleton component
+export { default as Skeleton } from './Skeleton';
+
+// Specific skeleton components
+export { default as SkeletonBadge } from './SkeletonBadge';
+export { default as SkeletonSprite } from './SkeletonSprite';
+export { default as SkeletonArtwork } from './SkeletonArtwork';
+export { default as SkeletonText } from './SkeletonText';
+export { default as SkeletonCard } from './SkeletonCard';
+export { default as SkeletonInteractiveLink } from './SkeletonInteractiveLink';
+export { default as SkeletonFilterButtons } from './SkeletonFilterButtons';
+
+// Preset compositions
+export { default as SkeletonPokedexGrid } from './SkeletonPokedexGrid';
+export { default as SkeletonGenerationGrid } from './SkeletonGenerationGrid';
+export { default as SkeletonPokedexPage } from './SkeletonPokedexPage';
+
+// Export default as the base Skeleton component
+export { default } from './Skeleton';
