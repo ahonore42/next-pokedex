a7bf2caa199e42719a8d261c22c2cb6df6aa74fd
ahonore42 <a1honore80@gmail.com>
2025-08-01 05:22:32 -0700

    feat(ui): add index file for searchbars module
    Adds a new index.ts file to the searchbars module. This file exports the SearchBar component and its related components and types, improving the organization and discoverability of the module.

---
+++ src/components/ui/searchbars/index.ts
@@ -0,0 +1,10 @@
+import SearchBar from './SearchBar';
+export { default as PokemonSearchResultRow } from './PokemonSearchResultRow';
+
+// Types
+export type { PokemonSearchResult } from './searchbar.config';
+
+// Render functions
+export { renderPokemonResult } from './PokemonSearchResultRow';
+
+export default SearchBar;
